732567f20f9f5fc5d6ec2e76861ebac58c17b0d9
Lívia Castilholi <33629714+LilySny@users.noreply.github.com>
2021-03-30 15:32:58 -0300

    added theme2

--- lib/shared/theme/app_theme.dart
+++ lib/shared/theme/app_theme.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'app_theme2.dart';
 
 class AppTheme {
 
     static ThemeData appTheme(){
-      return ThemeData(
-          primaryColor: Colors.pink[400],
-          accentColor: Colors.purple[200],
-          dividerColor: Colors.pink[400],
-          primaryColorBrightness: Brightness.dark,
-          accentColorBrightness: Brightness.dark,
-          backgroundColor: Colors.purple[900],
-          buttonColor: Colors.pink[400],
-          bottomAppBarColor: Colors.black54
-      );
+      return Theme2.appTheme2();
     }
 }
+
+
+    // static ThemeData appTheme(){
+    //   return ThemeData(
+    //       primaryColor: Colors.pink[400],
+    //       accentColor: Colors.purple[200],
+    //       dividerColor: Colors.pink[400],
+    //       primaryColorBrightness: Brightness.dark,
+    //       accentColorBrightness: Brightness.dark,
+    //       backgroundColor: Colors.purple[900],
+    //       buttonColor: Colors.pink[400],
+    //       bottomAppBarColor: Colors.black54
+    //   );
+    // }
---
+++ lib/shared/theme/app_theme2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Theme2 {
+  static ThemeData appTheme2() {
+    return ThemeData(
+        primaryColor: Colors.pink[400],
+        accentColor: Colors.purple[200],
+        dividerColor: Colors.pink[400],
+        primaryColorBrightness: Brightness.dark,
+        accentColorBrightness: Brightness.dark,
+        backgroundColor: Colors.purple[900],
+        buttonColor: Colors.pink[400],
+        bottomAppBarColor: Colors.black54);
+  }
+}
